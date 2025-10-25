4d8dfddcf5cb63877914521bfaf369040feb3b19
Sun Tao <2605127667@qq.com>
2025-10-25 17:49:16 +0800

    update

--- hello_agents/agents/__init__.py
+++ hello_agents/agents/__init__.py
@@ -1,12 +1,14 @@
 """Agent实现模块 - HelloAgents原生Agent范式"""
 
 from .simple_agent import SimpleAgent
+from .function_call_agent import FunctionCallAgent
 from .react_agent import ReActAgent
 from .reflection_agent import ReflectionAgent
 from .plan_solve_agent import PlanAndSolveAgent
 
 __all__ = [
     "SimpleAgent",
+    "FunctionCallAgent",
     "ReActAgent",
     "ReflectionAgent",
     "PlanAndSolveAgent"
